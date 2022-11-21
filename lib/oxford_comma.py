ca0367c21ac8303a270c13f522c6a8b11c6c8282
sbalayan1 <balayans2014@yahoo.com>
2022-11-21 09:28:12 -0800

    complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if (len(items) == 1): return ''.join(items)
+    if (len(items) == 2): return ' and '.join(items)
+    output = ''
+    for item in items:
+        if item == items[-1]:
+            output += f"and {item}"
+        else:
+            output += f"{item}, "
+    
+    return output
